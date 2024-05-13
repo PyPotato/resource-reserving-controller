517f130c1ac18faa75e3547a0d7207cf5491ce2d
zsj <sjzhou2000@163.com>
2024-05-13 16:34:20 +0000

    低效开发ing...
    Signed-off-by: zsj <sjzhou2000@163.com>

--- go.mod
+++ go.mod
@@ -5,19 +5,16 @@ go 1.22.0
 toolchain go1.22.3
 
 require (
-	github.com/docker/distribution v2.8.3+incompatible
-	k8s.io/apiextensions-apiserver v0.30.0
+	k8s.io/apimachinery v0.30.0
 	k8s.io/client-go v0.30.0
-	k8s.io/component-base v0.30.0
 	k8s.io/klog/v2 v2.120.1
+	sigs.k8s.io/controller-runtime v0.18.2
 )
 
 require (
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/blang/semver/v4 v4.0.0 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
@@ -32,18 +29,13 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/prometheus/client_golang v1.16.0 // indirect
-	github.com/prometheus/client_model v0.4.0 // indirect
-	github.com/prometheus/common v0.44.0 // indirect
-	github.com/prometheus/procfs v0.10.1 // indirect
-	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/oauth2 v0.10.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
@@ -54,7 +46,6 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.30.0 // indirect
-	k8s.io/apimachinery v0.30.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
--- pkg/resourcereserve/controller.go
+++ pkg/resourcereserve/controller.go
@@ -1,20 +1,28 @@
 package resourcereserve
 
 import (
+	"context"
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	informer "k8s.io/client-go/informers/core/v1"
 	"k8s.io/client-go/kubernetes"
 	coreLister "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 const (
-	workNum = 3
-	maxRetry = 10
-	namespace = "reservation"
+	workNum       = 3
+	maxRetry      = 10
+	namespace     = "reservation"
+	finalizerName = "reserve.kubernetes.io/finalizer"
 )
 
 type controller struct {
@@ -23,24 +31,181 @@ type controller struct {
 	queue     workqueue.RateLimitingInterface
 }
 
-func (c *controller) updatePod(old, new interface{}) {
-	
+func (c *controller) reconcile(key string) error {
+	podNamespace, podName, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+
+	pod, err := c.podLister.Pods(podNamespace).Get(podName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+
+	// DELETION
+	if pod.ObjectMeta.DeletionTimestamp.IsZero() {
+		klog.Info("Not being deleted")
+		if !controllerutil.ContainsFinalizer(pod, finalizerName) {
+			klog.Info("Adding finalizer")
+			controllerutil.AddFinalizer(pod, finalizerName)
+			if _, err := c.updatePod(pod); err != nil {
+				return err
+			}
+		}
+	} else {
+		// The object is being deleted
+		klog.Info("Being deleted")
+		if controllerutil.ContainsFinalizer(pod, finalizerName) {
+			klog.Info("Reserving resources")
+			if err := c.handlePodDelete(pod); err != nil {
+				return err
+			}
+			controllerutil.RemoveFinalizer(pod, finalizerName)
+			if _, err := c.updatePod(pod); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	// TODO ADD & UPDATE
+
+
+	return nil
 }
 
-func (c *controller) deletePod(obj interface{}) {
+func (c *controller) handlePodDelete(pod *corev1.Pod) error {
+	// TODO Add annotations according to resources required
 
+	return nil
 }
 
+func (c *controller) updatePod(pod *corev1.Pod) (*corev1.Pod, error) {
+	return c.client.CoreV1().Pods(pod.Namespace).Update(context.Background(), pod, metav1.UpdateOptions{})
+}
 
+func (c *controller) handleErr(key string, err error) {
+	// Enqueue & retry
+	if c.queue.NumRequeues(key) <= maxRetry {
+		c.queue.AddRateLimited(key)
+		return
+	}
+
+	runtime.HandleError(err)
+	c.queue.Forget(key)
+}
+
+func (c *controller) isReservable(obj interface{}) bool {
+	// Get labels and judge if exists label with key == "reserve.kubernetes.io/resources"
+	metaObj, ok := obj.(metav1.Object)
+	if !ok {
+		return false
+	}
+
+	labels := metaObj.GetLabels()
+	_, found := labels["reserve.kubernetes.io/resources"]
+
+	return found
+}
+
+func (c *controller) judgeDeployment(ownerRef metav1.OwnerReference, namespace string) bool {
+	deployment, err := c.client.AppsV1().Deployments(namespace).Get(context.Background(), ownerRef.Name, metav1.GetOptions{})
+	if err != nil {
+		klog.Error("Failed to get deployment", err)
+		return false
+	}
+	// pod 被删除并且 readyReplicas < Replicas —— pod重建
+	if *deployment.Spec.Replicas > deployment.Status.ReadyReplicas {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *controller) judgeStatefulSet(ownerRef metav1.OwnerReference, namespace string) bool {
+	statefulset, err := c.client.AppsV1().StatefulSets(namespace).Get(context.Background(), ownerRef.Name, metav1.GetOptions{})
+	if err != nil {
+		klog.Error("Failed to get statefulset", err)
+		return false
+	}
+
+	if *statefulset.Spec.Replicas > statefulset.Status.ReadyReplicas {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *controller) isReconstruction(obj interface{}) bool {
+	metaObj, ok := obj.(metav1.Object)
+	if !ok {
+		return false
+	}
+
+	if len(metaObj.GetOwnerReferences()) > 0 {
+		// 过滤销毁情况
+		for _, ownerRef := range metaObj.GetOwnerReferences() {
+			switch ownerRef.Kind {
+			case "Deployment":
+				return c.judgeDeployment(ownerRef, metaObj.GetNamespace())
+			case "StatefulSet":
+				return c.judgeStatefulSet(ownerRef, metaObj.GetNamespace())
+			}
+		}
+	}
+
+	return false
+}
+
+func (c *controller) enqueue(obj interface{}) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+	}
+	c.queue.Add(key)
+}
+
+func (c *controller) onPodAdded(obj interface{}) {
+	if !c.isReservable(obj) {
+		return
+	}
+	c.enqueue(obj)
+}
+
+func (c *controller) onPodDeleted(obj interface{}) {
+	// ! 入队条件是否有遗漏？
+	// 只有打上特定标签的的 pod 才是本 controller 关心的 pod
+	if !c.isReservable(obj) {
+		return
+	}
+	// 过滤掉裸 pod 以及 pod 销毁情况
+	if !c.isReconstruction(obj) {
+		return
+	}
+
+	c.enqueue(obj)
+}
+
+func (c *controller) onPodUpdated(oldObj interface{}, newObj interface{}) {
+	// 成功调度后 pod 会更新 spec.nodeName
+	if !c.isReservable(oldObj) || !c.isReservable(newObj) {
+		return
+	}
+	c.enqueue(newObj)
+}
 
 func (c *controller) Run(stopCh <-chan struct{}) {
-	for i := 0; i < workNum; i ++ {
+	for i := 0; i < workNum; i++ {
 		go wait.Until(c.worker, time.Minute, stopCh)
 	}
 }
 
 func (c *controller) worker() {
-	for c.processNextItem() {}
+	for c.processNextItem() {
+	}
 }
 
 func (c *controller) processNextItem() bool {
@@ -52,7 +217,7 @@ func (c *controller) processNextItem() bool {
 
 	key := item.(string)
 
-	//
+	// 调谐主逻辑
 	err := c.reconcile(key)
 	if err != nil {
 		c.handleErr(key, err)
@@ -60,27 +225,17 @@ func (c *controller) processNextItem() bool {
 	return true
 }
 
-func (c *controller) reconcile(key string) error {
-	//TODO 调谐逻辑
-
-	return nil
-}
-
-func (c *controller) handleErr(key string, err error) {
-	//TODO Enqueue & retry
-	
-}
-
 func NewController(client kubernetes.Interface, podInformer informer.PodInformer) controller {
-	c := controller {
-		client: client,
+	c := controller{
+		client:    client,
 		podLister: podInformer.Lister(),
-		queue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "resourcereserve"),
+		queue:     workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "resourcereserve"),
 	}
 
 	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		UpdateFunc: c.updatePod,
-		DeleteFunc: c.deletePod,
+		AddFunc: c.onPodAdded,
+		DeleteFunc: c.onPodDeleted,
+		UpdateFunc: c.onPodUpdated,
 	})
 
 	return c
